4ce941993db7f5982462081c4b76d96cd2302d5e
Baba0608 <babafakruddinnitturu@gmail.com>
2024-02-12 17:12:26 +0530

    orders page

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { Header } from "./Header";
+import { Shimmer } from "./Shimmer";
+
+const BACKEND_API = process.env.REACT_APP_BACKEND_API;
 
 export const Orders = () => {
+  const [myOrders, setMyOrders] = useState(null);
+
+  const fetchOrders = async () => {
+    const { data } = await axios.get(`${BACKEND_API}/order/get-all-orders`, {
+      headers: { authorization: localStorage.getItem("store-token") },
+    });
+
+    setMyOrders(data.orders);
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  if (!myOrders) {
+    return <Shimmer />;
+  }
+
   return (
     <div>
       <Header />
+
+      <div>
+        <p>My orders</p>
+      </div>
     </div>
   );
 };
